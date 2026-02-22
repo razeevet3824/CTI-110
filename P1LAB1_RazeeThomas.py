28a4c35bf5eca6daa71c544fbd16f09963179e75
razeevet3824 <razeevet3824@student.faytechcc.edu>
2026-02-22 09:58:47 -0500

    Add files via upload

---
+++ P1LAB1_RazeeThomas.py
@@ -0,0 +1,9 @@
+# Thomas Razee
+# February 22, 2026
+# P1LAB1
+# Getting Input From Users
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
