d2d5c9d4516a3bb2ea9790e364838f82d44d3e0c
razeevet3824 <razeevet3824@student.faytechcc.edu>
2026-02-28 21:39:00 -0500

    Add files via upload

---
+++ P1HW1_RazeeThomas.py
@@ -0,0 +1,22 @@
+# Thomas Razee
+# February 28, 2026
+# P1HW1
+# To write code that does basic math by collecting information from users, processing that information, and displaying an result of those.
+
+
+print("----Calculating Exponents-----")
+print()
+print()
+base_value = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print()
+print()
+print(base_value, "raised to the power of", exponent, "is", base_value**exponent, "!!")
+print()
+print()
+print("-----Addition and Subtraction----")
+my_num = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print()
+print(my_num, "+", add, "-", subtract, "is equal to", my_num + add - subtract)
